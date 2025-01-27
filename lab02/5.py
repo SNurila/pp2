78e84731ce007dab258fc8324f8da1c18fab2b51
SNurila <salamatnurila@gmail.com>
2025-01-27 13:50:07 +0500

    fifth commit

---
+++ lab02/5.py
@@ -0,0 +1,13 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"] #Change the values "banana" and "cherry" with the values "blackcurrant" and "watermelon"
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"] #Change the second value by replacing it with two new values
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"] #Change the second and third value by replacing it with one value
+print(thislist)
