4bde2a62ac9fe8e37c89766878af00daadb2540c
SNurila <salamatnurila@gmail.com>
2025-01-27 13:52:53 +0500

    sixth commit

---
+++ lab02/6.py
@@ -0,0 +1,8 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange") #Insert an item as the second position
+print(thislist)
