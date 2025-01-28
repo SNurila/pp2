b19ff1f01b56c283e91233abb3deb1ced6f62ee6
SNurila <salamatnurila@gmail.com>
2025-01-28 20:50:02 +0500

    13th commit

---
+++ lab02/13.py
@@ -0,0 +1,18 @@
+fruits = ['apple', 'banana', 'cherry']
+
+x = fruits.index("cherry")
+print(x)
+
+
+fruits = ['apple', 'banana', 'cherry']
+
+points = (1, 4, 5, 9)
+
+fruits.extend(points)
+print(fruits)
+
+
+fruits = ['apple', 'banana', 'cherry']
+
+x = fruits.pop(1)
+print(x)
