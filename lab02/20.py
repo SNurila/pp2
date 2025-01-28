33ad180201b6be698ea49fb78e3fb17bccfeef73
SNurila <salamatnurila@gmail.com>
2025-01-28 21:28:43 +0500

    20th commit

---
+++ lab02/20.py
@@ -0,0 +1,11 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+# Note: the set list is unordered, meaning: the items will appear in a random order.
+
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry", False, True, 0}
+
+print(thisset)
