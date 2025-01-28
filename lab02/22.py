e971bd9953df61c33eaf07e8e41c598d2dc9ed1d
SNurila <salamatnurila@gmail.com>
2025-01-28 21:32:24 +0500

    22ndt commit

---
+++ lab02/22.py
@@ -0,0 +1,21 @@
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical) #Add elements from tropical into thisset
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
