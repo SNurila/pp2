aaa7d23dc81239fa1d196b04f7b576079356ae73
SNurila <salamatnurila@gmail.com>
2025-02-15 17:18:52 +0500

    4th commit

---
+++ lab04/w3school/modules.py
@@ -0,0 +1,10 @@
+import mymodule  #Import the module named mymodule, and call the greeting function
+
+mymodule.greeting("Jonathan")
+
+
+
+a = mymodule.person1["age"]  #mport the module named mymodule, and access the person1 dictionary:
+print(a)
+
+
---
+++ lab04/w3school/mymodule.py
@@ -0,0 +1,10 @@
+def greeting(name):  #Save this code in a file named mymodule.py
+  print("Hello, " + name)
+
+
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
