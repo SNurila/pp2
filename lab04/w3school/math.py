f7ed7b188aa9dd535cbe43d1929122f4fd4a0114
SNurila <salamatnurila@gmail.com>
2025-02-15 17:41:14 +0500

    9th commit

---
+++ lab04/w3school/math.py
@@ -0,0 +1,19 @@
+'''
+Built-in functions
+The min() and max() functions can be used to find the lowest or highest value in an iterable
+'''
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
+x = abs(-7.25) #The abs() function returns the absolute (positive) value of the specified number
+
+print(x)
+
+x = pow(4, 3) #The pow(x, y) function returns the value of x to the power of y (xy)
+
+print(x)
