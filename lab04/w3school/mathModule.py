22dc6e990ea72d4191929c295828110a8d503ed7
SNurila <salamatnurila@gmail.com>
2025-02-15 17:42:38 +0500

    10th commit

---
+++ lab04/w3school/mathModule.py
@@ -0,0 +1,15 @@
+import math
+x = math.sqrt(64)
+print(x)
+
+
+x = math.ceil(1.4) #math.ceil() method rounds a number upwards to its nearest integer,
+y = math.floor(1.4) #math.floor() method rounds a number downwards to its nearest integer
+
+print(x) # returns 2
+print(y) # returns 1
+
+
+x = math.pi
+
+print(x)
