46b631faf304d82ae2967468bacc79a012e6704f
SNurila <salamatnurila@gmail.com>
2025-02-15 17:06:39 +0500

    2nd commit

--- lab04/w3school/itgen_01.py
+++ lab04/w3school/itgen_01.py
@@ -16,3 +16,28 @@ print(next(myiter))
 print(next(myiter))
 print(next(myiter))
 print(next(myiter))
+
+
+
+
+class MyNumbers:  #stop iteration
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
+
+
+  
