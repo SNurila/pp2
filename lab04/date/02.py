704a487457c0dd3fb6a3af01d27e86daedcf2a9a
SNurila <salamatnurila@gmail.com>
2025-02-21 16:32:14 +0500

    2nd commit

---
+++ lab04/date/02.py
@@ -0,0 +1,8 @@
+from datetime import datetime, timedelta
+today = datetime.today()
+yesterday = today - timedelta(days=1)
+tomorrow = today + timedelta(days=1)
+print(yesterday.strftime('%Y-%m-%d'))
+print(today.strftime('%Y-%m-%d'))
+print(tomorrow.strftime('%Y-%m-%d'))
+
