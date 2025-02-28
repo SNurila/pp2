8b05d27e21687e0e25727affd564b8e4be86e284
SNurila <salamatnurila@gmail.com>
2025-02-28 20:54:27 +0500

    19th commit

---
+++ lab05/w3schools/19.py
@@ -0,0 +1,26 @@
+import re
+
+txt = "The rain in Spain"
+
+#Check if "ain" is present, but NOT at the beginning of a word:
+
+x = re.findall(r"\Bain", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+
+#Check if "ain" is present, but NOT at the end of a word:
+
+x = re.findall(r"ain\B", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
