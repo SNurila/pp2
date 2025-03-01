57a805de269a6b15136f75bec9798f5af53405f9
SNurila <salamatnurila@gmail.com>
2025-03-01 18:03:57 +0500

    15th commit

---
+++ lab06/w3school/built-in functions/15.py
@@ -0,0 +1,9 @@
+class Person:
+  name = "John"
+  age = 36
+  country = "Norway"
+
+print(dir(Person))
+#The dir() function returns all properties and methods of the specified object, without the values.
+#This function will return all the properties and methods, even built-in properties which are default for all object.
+
