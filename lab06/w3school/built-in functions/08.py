8e829c46983c278e4c7805d31c3e4bfb09e97ac6
SNurila <salamatnurila@gmail.com>
2025-03-01 17:20:57 +0500

    8th commit

---
+++ lab06/w3school/built-in functions/08.py
@@ -0,0 +1,4 @@
+x = bytes(4)
+print(x)
+#The difference between bytes() and bytearray() is that bytes() returns an object 
+# that cannot be modified, and bytearray() returns an object that can be modified.
