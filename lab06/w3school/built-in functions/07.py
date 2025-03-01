0250d35962019bf96edc7f2f2a67570a71f8e141
SNurila <salamatnurila@gmail.com>
2025-03-01 17:19:32 +0500

    7th commit

---
+++ lab06/w3school/built-in functions/07.py
@@ -0,0 +1,4 @@
+x = bytearray(4) #Return an array of 4 bytes
+print(x)
+#The bytearray() function returns a bytearray object.
+#It can convert objects into bytearray objects, or create empty bytearray object of the specified size.
