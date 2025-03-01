7f1abbdef29da610b9856adc063c03d011efbb8e
SNurila <salamatnurila@gmail.com>
2025-03-01 17:16:33 +0500

    6th commit

---
+++ lab06/w3school/built-in functions/06.py
@@ -0,0 +1,11 @@
+'''
+The bool() function returns the boolean value of a specified object.
+The object will always return True, unless:
+The object is empty, like [], (), {}
+The object is False
+The object is 0
+The object is None
+
+'''
+x = bool(1)
+print(x)
